5ed1c6646354f25f4ba2002041e93c2ae8e12797
90534 <90534@ALI>
2025-02-09 21:53:59 +0300

    Context class oluşturuldu

---
+++ Infrastructure/BlogApi.Persistence/Context/BlogContext.cs
@@ -0,0 +1,24 @@
+﻿using BlogApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Persistence.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ALI\\SQLEXPRESS;Database=myDataBase;Trusted_Connection=True;");
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
+
