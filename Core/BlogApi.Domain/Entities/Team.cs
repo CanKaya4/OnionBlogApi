0d2cbedafa0e65b41f6b62bd133b4b549f1c352c
90534 <90534@ALI>
2025-02-09 21:37:14 +0300

    Temel Entity'lerin oluşturulması

---
+++ Core/BlogApi.Domain/Entities/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain.Entities
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biopraphy { get; set; }
+    }
+}
